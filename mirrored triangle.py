8a269ae67b5a29e02542df0390407f4b64807d8d
shehryar1846 <landcrane361@gmail.com>
2026-03-20 16:36:13 +0500

    Hi

---
+++ mirrored triangle.py
@@ -0,0 +1,12 @@
+import turtle
+t=turtle.Turtle()
+t.pensize(3)
+t.color("cyan")
+leg_lenght=180
+t.left(120)
+t.forward(leg_lenght)
+t.right(120)
+t.forward(leg_lenght)
+t.right(120)
+t.forward(leg_lenght)
+turtle.done()
